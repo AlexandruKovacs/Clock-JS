9a6f8d56edde1f75d35d75bdaabf68aa3f384eaa
Alexandru Kovacs <alexkovacs0045@gmail.com>
2025-10-25 15:14:01 +0200

    que pille el .js

--- js/script.js
+++ js/script.js
@@ -19,6 +19,8 @@ document.getElementById("contact-form").addEventListener("submit", async (e) => 
   e.preventDefault();
   const formData = Object.fromEntries(new FormData(e.target).entries());
   
+  
+  
   const res = await fetch("/.netlify/functions/send-email", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
