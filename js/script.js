87202bc2b94891d10bdcb4e6571d214e653f45b8
Alexandru Kovacs <alexkovacs0045@gmail.com>
2025-10-25 14:59:44 +0200

    ajuste ruta

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ document.getElementById("contact-form").addEventListener("submit", async (e) => 
   e.preventDefault();
   const formData = Object.fromEntries(new FormData(e.target).entries());
   
-  const res = await fetch("/.netlify/functions/send-email", {
+  const res = await fetch("/.netlify/functions/send-email.js", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(formData),
